8337f2269d3b29f145628150d902e750671b2f72
A.Badmaev <ABadmaev@mail.ru>
2019-11-21 19:18:38 +0900

    3 zadanie

--- hello3.py
+++ hello3.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', name="TEST")
+    return render_template('index3.html', name=random.randint(1,100))
