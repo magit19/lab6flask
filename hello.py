a0db15fc91e737365d0ec1411b0fd1fa5810c2db
porotov <vs77.porotov>
2019-11-21 18:57:11 +0900

    2

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from flask import Flask
-
+import random
 app = Flask(__name__)
-
+#a = random.random() * 100
+a=random.randint(1,99)
 @app.route('/')
 def hello_world():
-    return '<h1>Hello world!</h1><p>Случайное число:</p>'
+    return f'<h1>Hello world!</h1><p>Поротов</p><p>Случайное число:{a}</p>'
