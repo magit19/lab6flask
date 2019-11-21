8c03e4c21f678e86f9991d65080a28c13c5072ab
poewmuh <asd@asd.asd>
2019-11-21 18:40:09 +0900

    random

--- hello.py
+++ hello.py
@@ -1,11 +1,9 @@
 from flask import Flask
 import random
 
-
 app = Flask(__name__)
-
+a = random.random() * 100
 @app.route('/')
 def hello_world():
-	a = random.random()
-    return "<h1> LAB1 </h1> <p>Счастливое число:</p>"
+    return f"<h1> LAB1 </h1> <p>Счастливое число: {a}</p>"
     		
