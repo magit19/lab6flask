ea0fdc6406985e405b3273d7caaf9bebca68dbaa
ilya <ilya@mail.ru>
2019-12-23 15:48:44 +0900

    sdelal

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
-
+a = random.random() * 100
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return f"<h1> LAB1 </h1> <p>Случайное число: {a}</p>"
+    		
--- hello3.py
+++ hello3.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', name="TEST")
+    return render_template('index1.html', name=random.randint(0,100))
